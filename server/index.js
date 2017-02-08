7227d855d50551e7cc731b79122153b51022cf76
Edward Park <ed.sh.park@gmail.com>
2017-02-08 15:12:08 -0800

    importing morgan + userRoute gateway

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
+const logger = require('morgan');
 
 const app = express();
 
@@ -9,18 +10,21 @@ const app = express();
 // maybe put this into an app.use()?
 const { contactJobs } = require('./services/jobConsumer/consumer');
 
+app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }), bodyParser.json());
 app.use(express.static(path.join(__dirname, '../src')));
 
 // Service-based routing import
-const contactRoute = require('./services/contacts/contactRoute');
+const contactRoutes = require('./services/contacts/contactRoutes');
+const userRoutes = require('./services/users/userRoutes');
 
 // REST Gateways
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
-app.use('/contact', contactRoute);
+app.use('/contact', contactRoutes);
+app.use('/user', userRoutes);
 
 // Error catch-all
 app.all('*', (req, res) => {
