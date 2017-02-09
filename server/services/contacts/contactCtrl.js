301036dc48cc752ee578982949e2aa2f1b428f57
Edward Park <ed.sh.park@gmail.com>
2017-02-08 16:50:12 -0800

    added contactModel + saveContact middleware function

--- server/services/contacts/contactCtrl.js
+++ server/services/contacts/contactCtrl.js
@@ -1,3 +1,4 @@
+const db = require('./contactModel');
 const kue = require('kue');
 
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379/0';
@@ -8,7 +9,7 @@ const queue = kue.createQueue({
 
 const contactCtrl = {};
 
-contactCtrl.submitForm = (req, res) => {
+contactCtrl.submitForm = (req, res, next) => {
   const { name, email, phone, message } = req.body;
 
   const job = queue.create('contact', {
@@ -29,8 +30,22 @@ contactCtrl.submitForm = (req, res) => {
       job.on('complete', () => { console.log(`job for ${req.body.email} complete!`); });
       job.on('failed', () => { console.error(`job for ${req.body.email} failed!`); });
 
-      return res.status(201).end();
+      // return res.status(201).end();
+      next();
     });
+};
+
+contactCtrl.saveContact = (req, res) => {
+  const { name, email, phone, message } = req.body;
+
+  db.query('insert into contacts (name, email, phone, message) values ($1, $2, $3, $4)', [name, email, phone, message], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(400).end();
+    }
+
+    return res.status(201).end();
+  });
 };
 
 module.exports = contactCtrl;
