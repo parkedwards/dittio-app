ed27f67fa9df5b94966a3ecacd5eabde6f4a9746
Edward Park <ed.sh.park@gmail.com>
2017-02-08 16:50:18 -0800

    created contactModel

---
+++ server/services/contacts/contactModel.js
@@ -0,0 +1,15 @@
+const db = require('../../database/database');
+
+db.query(`create table if not exists "contacts" (
+  _id serial primary key,
+  name varchar(256) not null,
+  email varchar(256) not null,
+  phone varchar(256,
+  message varchar(256)
+)`, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
+
+module.exports = db;
