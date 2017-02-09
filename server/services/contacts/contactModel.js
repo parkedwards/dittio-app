ecc8267e38f3f076127f5998c82172a2de7cde32
Edward Park <ed.sh.park@gmail.com>
2017-02-08 17:28:00 -0800

    fixed syntax error in contactModel

--- server/services/contacts/contactModel.js
+++ server/services/contacts/contactModel.js
@@ -4,7 +4,7 @@ db.query(`create table if not exists "contacts" (
   _id serial primary key,
   name varchar(256) not null,
   email varchar(256) not null,
-  phone varchar(256,
+  phone varchar(256),
   message varchar(256)
 )`, (err) => {
   if (err) {
