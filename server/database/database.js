1709856bf26de559756360326f79e17fa9b2e724
Edward Park <ed.sh.park@gmail.com>
2017-02-11 17:11:02 -0800

    importing PG_CONFIG from config object

--- server/database/database.js
+++ server/database/database.js
@@ -1,17 +1,8 @@
 const pg = require('pg');
-
-const { PG_USER, PG_PW, PG_DB, PG_HOST, PG_PORT, PG_MAX, PG_IDLE } = process.env;
+const { PG_CONFIG } = require('../config');
 
 // maybe destructure
-const pool = new pg.Pool({
-  user: PG_USER,
-  password: PG_PW,
-  database: PG_DB,
-  host: PG_HOST,
-  port: PG_PORT,
-  max: PG_MAX,
-  idleTimeoutMillis: PG_IDLE,
-});
+const pool = new pg.Pool(PG_CONFIG);
 
 pool.connect((err) => {
   if (err) console.error(err);
