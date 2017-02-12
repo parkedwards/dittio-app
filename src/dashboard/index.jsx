93b13bf808281a3e2d764b7be9dc9b9a7c1e7293
Edward Park <ed.sh.park@gmail.com>
2017-02-11 19:08:08 -0800

    scaffolded dashboard React entry

---
+++ src/dashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import { Root } from './containers';
+import store from './store/index';
+
+render(
+  <Root store={store} />,
+  document.querySelector('#app'),
+);
+
+
