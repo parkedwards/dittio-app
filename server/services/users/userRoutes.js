e8c34de261bebdc13883df564c9e453c2066abd8
Edward Park <ed.sh.park@gmail.com>
2017-02-16 13:01:20 -0800

    added test logging for user service routing endpoints

--- server/services/users/userRoutes.js
+++ server/services/users/userRoutes.js
@@ -3,5 +3,10 @@ const userCtrl = require('./userCtrl');
 
 const userRoute = module.exports = new Router();
 
-userRoute.post('/login');
-userRoute.post('/signup');
+userRoute.post('/login', (req, res) => {
+  console.log('/login route was hit!');
+});
+
+userRoute.post('/signup', (req, res) => {
+  console.log('/signup route was hit!');
+});
