09b880925d2fc4eafab9fec213082ade1c730362
Edward Park <ed.sh.park@gmail.com>
2017-04-01 15:50:25 -0700

    initial integration of firebase to user service

--- server/services/users/userRoutes.js
+++ server/services/users/userRoutes.js
@@ -1,10 +1,22 @@
 const { Router } = require('express');
 const userCtrl = require('./userCtrl');
+const admin = require('firebase-admin');
 
 const userRoute = new Router();
 
 module.exports = (passport) => {
-  userRoute.post('/login', passport.authenticate('local-login'), userCtrl.loginUser);
+  // userRoute.post('/login', passport.authenticate('local-login'), userCtrl.loginUser);
+  userRoute.post('/login', (req, res) => {
+
+    console.log('login route was hit!');
+
+    const { email, password } = req.body;
+
+    admin.auth().getUserByEmail(email)
+      .then((retrievedUser) => {
+        console.log(retrievedUser.toJSON());
+      });
+  });
 
   userRoute.post('/', (req, res) => {
     console.log('/signup route was hit!');
