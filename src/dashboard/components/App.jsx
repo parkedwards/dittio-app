8d25314642c318cc99e130fd720b81b3393e4f04
Edward Park <ed.sh.park@gmail.com>
2017-04-03 12:00:58 -0700

    Adding logout button to App.js

--- src/dashboard/components/App.jsx
+++ src/dashboard/components/App.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
 
 import TopNav from './TopNav';
 import LeftNav from './LeftNav';
+import Button from './Button';
 
-const App = () => (
-  <div>
-    <TopNav />
-    <LeftNav />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onLogoutPress() {
+    fetch('/user/logout', {
+      method: 'POST',
+    })
+      .then(() => {
+        window.location.href = '/';
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <TopNav />
+        <LeftNav />
+        <Button onPress={this.onLogoutPress}>Log Out</Button>
+      </div>
+    );
+  }
+}
 
 export default App;
+
