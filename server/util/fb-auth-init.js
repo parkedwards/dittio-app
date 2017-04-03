789fd854e68314735bdb77001619fff4424a84d9
Edward Park <ed.sh.park@gmail.com>
2017-04-03 12:00:51 -0700

    switch FB init to access standard FB auth (vs. admin)

--- server/util/fb-auth-init.js
+++ server/util/fb-auth-init.js
@@ -1,12 +1,9 @@
 // Initializes a connection to the Firebase DB
 
-const admin = require('firebase-admin');
-const { SERVICE_ACCOUNT_KEY } = require('../config');
+const firebase = require('firebase');
+const { FB_CONFIG } = require('../config');
 
 module.exports = () => {
-  admin.initializeApp({
-    credential: admin.credential.cert(SERVICE_ACCOUNT_KEY),
-    databaseURL: 'https://dittio-users.firebaseio.com',
-  });
+  firebase.initializeApp(FB_CONFIG);
 };
 
